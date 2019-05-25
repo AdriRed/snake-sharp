067375db2efe3dc26f87515716c9b82a61b9700c
AdriRed <adriamilmarcos@gmail.com>
2019-05-25 19:18:29 +0200

    Initial blank project
    It has nothing but "Hello world!" msg

---
+++ snake-sharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace snake_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
